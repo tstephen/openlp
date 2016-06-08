df82628c0104b65eb0442637fecd7d2d5caf26a7
Tomas Groth <second@tgc.dk>
2016-06-08 21:34:29 +0200

    Fix merge issue

--- openlp/core/lib/serviceitem.py
+++ openlp/core/lib/serviceitem.py
@@ -34,7 +34,7 @@ import ntpath
 from PyQt5 import QtGui
 
 from openlp.core.common import RegistryProperties, Settings, translate, AppLocation, md5_hash
-from openlp.core.lib import ImageSource, build_icon, clean_tags, expand_tags, expand_chords
+from openlp.core.lib import ImageSource, build_icon, clean_tags, expand_tags, expand_chords, create_thumb
 
 log = logging.getLogger(__name__)
 
