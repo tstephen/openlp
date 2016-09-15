25631152ad84d83275aeb18efa58a6c6ead34a57
Olli Suutari <suutari.olli@gmail.com>
2016-09-15 03:22:36 +0300

    The import saves the value if we use QLineEdit instead of QPlainTextEdit... But why?

--- openlp/plugins/bibles/forms/bibleimportform.py
+++ openlp/plugins/bibles/forms/bibleimportform.py
@@ -381,7 +381,7 @@ class BibleImportForm(OpenLPWizard):
         self.full_license_label = QtWidgets.QLabel(self.license_details_page)
         self.full_license_label.setObjectName('FullLicenseLabel')
         self.license_details_layout.setWidget(3, QtWidgets.QFormLayout.LabelRole, self.full_license_label)
-        self.full_license_edit = QtWidgets.QPlainTextEdit(self.license_details_page)
+        self.full_license_edit = QtWidgets.QLineEdit(self.license_details_page)
         self.full_license_edit.setObjectName('FullLicenseEdit')
         self.license_details_layout.setWidget(3, QtWidgets.QFormLayout.FieldRole, self.full_license_edit)
         self.addPage(self.license_details_page)
@@ -750,7 +750,7 @@ class BibleImportForm(OpenLPWizard):
         self.setField('license_version', self.version_name_edit.text())
         self.setField('license_copyright', self.copyright_edit.text())
         self.setField('license_permissions', self.permissions_edit.text())
-        self.setField('license_full_license', self.full_license_edit.toPlainText())
+        self.setField('license_full_license', self.full_license_edit.text())
         self.on_web_source_combo_box_index_changed(WebDownload.Crosswalk)
         settings.endGroup()
 
