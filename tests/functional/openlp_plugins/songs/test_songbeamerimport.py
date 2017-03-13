e97c157a19b6a0b00bf5414c7fe3fff40bb01b94
Simon Hanna <simon.hanna@serve-me.info>
2017-03-13 13:43:55 +0100

    Add comment

--- tests/functional/openlp_plugins/songs/test_songbeamerimport.py
+++ tests/functional/openlp_plugins/songs/test_songbeamerimport.py
@@ -50,6 +50,9 @@ class TestSongBeamerFileImport(SongImportTestHelper):
                          self.load_external_result_data(os.path.join(TEST_PATH, 'Lobsinget dem Herrn.json')))
 
     def test_cp1252_encoded_file(self):
+        """
+        Test that a CP1252 encoded file get's decoded properly.
+        """
         self.file_import([os.path.join(TEST_PATH, 'cp1252song.sng')],
                          self.load_external_result_data(os.path.join(TEST_PATH, 'cp1252song.json')))
 
