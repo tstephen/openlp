b3669c4f5dd49c7b5ee2fc3033aedd7908854156
Raoul Snyman <raoul@snyman.info>
2017-11-30 15:46:03 -0700

    Change things around a bit

--- tests/functional/openlp_plugins/songs/test_openoffice.py
+++ tests/functional/openlp_plugins/songs/test_openoffice.py
@@ -22,20 +22,20 @@
 """
 This module contains tests for the OpenOffice/LibreOffice importer.
 """
-from unittest import TestCase, SkipTest
+from unittest import TestCase, skipIf
 from unittest.mock import MagicMock, patch
 
-from openlp.core.common import is_linux
-
-if not is_linux():
-    raise SkipTest('Could not import OpenOfficeImport probably due to unavailability of uno')
-
 from openlp.core.common.registry import Registry
-from openlp.plugins.songs.lib.importers.openoffice import OpenOfficeImport
 
 from tests.helpers.testmixin import TestMixin
 
+try:
+    from openlp.plugins.songs.lib.importers.openoffice import OpenOfficeImport
+except ImportError:
+    OpenOfficeImport = None
 
+
+@skipIf(OpenOfficeImport is None, 'Could not import OpenOfficeImport probably due to unavailability of uno')
 class TestOpenOfficeImport(TestCase, TestMixin):
     """
     Test the :class:`~openlp.plugins.songs.lib.importer.openoffice.OpenOfficeImport` class
