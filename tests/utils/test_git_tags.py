808ee8ed2df1b60d0ac2e9c39a1d02921ab1dfd4
Benjamin Hoving <benjamin.hoving@gmail.com>
2019-09-18 13:14:12 -0400

    cleanup/flake8

--- tests/utils/test_git_tags.py
+++ tests/utils/test_git_tags.py
@@ -22,7 +22,6 @@
 """
 Package to test for proper bzr tags.
 """
-import os
 from subprocess import PIPE, Popen
 from unittest import TestCase, SkipTest
 
@@ -32,39 +31,15 @@ TAGS1 = {'1.9.0', '1.9.1', '1.9.2', '1.9.3', '1.9.4', '1.9.5', '1.9.6', '1.9.7',
          '2.3.1', '2.3.2', '2.3.3', '2.4'}
 
 
-# class TestBzrTags(TestCase):
-
-#     def test_bzr_tags(self):
-#         """
-#         Test for proper bzr tags
-#         """
-#         # GIVEN: A bzr branch
-#         path = os.path.dirname(__file__)
-
-#         # WHEN getting the branches tags
-#         try:
-#             bzr = Popen(('bzr', 'tags', '--directory=' + path), stdout=PIPE)
-#         except Exception:
-#             raise SkipTest('bzr is not installed')
-#         std_out = bzr.communicate()[0]
-#         count = len(TAGS1)
-#         tags = [line.decode('utf-8').split()[0] for line in std_out.splitlines()]
-#         count1 = 0
-#         for t in tags:
-#             if t in TAGS1:
-#                 count1 += 1
-
-#         # THEN the tags should match the accepted tags
-#         assert count == count1, 'List of tags should match'
-
-
 class TestGitTags(TestCase):
     def test_git_tags(self):
         """
         Test for proper git tags
         """
         # GIVEN: a git repo
-        path = os.path.dirname(__file__)
+
+        # pyflake complains about this line because the value of path is never used
+        # path = os.path.dirname(__file__)
 
         # WHEN: getting the tags
         try:
