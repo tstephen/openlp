65f7af0e9f067915ef88e5ed922edb21b03ad410
Mattias Põldaru <mahfiaz@gmail.com>
2020-07-08 20:15:37 +0000

    Revert recently introduced bug causing video background to be on top of lyrics. The crash this was supposed to fix is no longer present.

--- openlp/core/display/window.py
+++ openlp/core/display/window.py
@@ -296,6 +296,7 @@ class DisplayWindow(QtWidgets.QWidget, RegistryProperties, LogMixin):
             return self.__script_result
         else:
             self.webview.page().runJavaScript(script)
+        self.raise_()
 
     def go_to_slide(self, verse):
         """
