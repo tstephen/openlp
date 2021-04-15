ac6aae9a3a46a01d7649270fbe35407276757fbe
Tim Bentley <tim.bentley@gmail.com>
2021-04-15 17:25:59 +0000

    Update Tests - 02

--- tests/openlp_core/ui/test_mainwindow.py
+++ tests/openlp_core/ui/test_mainwindow.py
@@ -28,11 +28,13 @@ from unittest.mock import MagicMock, patch
 from shutil import rmtree
 from tempfile import mkdtemp
 
-from PyQt5 import QtCore, QtWidgets
+from PyQt5 import QtCore, QtWidgets, QtGui
 
 from openlp.core.common.i18n import UiStrings
 from openlp.core.common.registry import Registry
 from openlp.core.display.screens import ScreenList
+from openlp.core.lib.plugin import PluginStatus
+from openlp.core.state import State
 from openlp.core.ui.mainwindow import MainWindow
 from tests.utils.constants import TEST_RESOURCES_PATH, RESOURCE_PATH
 
@@ -73,6 +75,32 @@ def main_window(state, settings, mocked_qapp):
     del mainwindow
     renderer_patcher.stop()
     add_toolbar_action_patcher.stop()
+
+
+@pytest.fixture()
+def main_window_reduced(settings, state):
+    """
+    Create the UI
+    """
+    Registry().set_flag('no_web_server', True)
+    mocked_plugin = MagicMock()
+    mocked_plugin.status = PluginStatus.Active
+    mocked_plugin.icon = QtGui.QIcon()
+    Registry().register('mock_plugin', mocked_plugin)
+    State().add_service("mock", 1, is_plugin=True, status=PluginStatus.Active)
+    # Mock classes and methods used by mainwindow.
+    with patch('openlp.core.ui.mainwindow.SettingsForm'), \
+            patch('openlp.core.ui.mainwindow.OpenLPDockWidget'), \
+            patch('openlp.core.ui.mainwindow.QtWidgets.QToolBox'), \
+            patch('openlp.core.ui.mainwindow.QtWidgets.QMainWindow.addDockWidget'), \
+            patch('openlp.core.ui.mainwindow.ServiceManager'), \
+            patch('openlp.core.ui.mainwindow.ThemeManager'), \
+            patch('openlp.core.ui.mainwindow.ProjectorManager'), \
+            patch('openlp.core.ui.mainwindow.HttpServer'), \
+            patch('openlp.core.ui.mainwindow.WebSocketServer'), \
+            patch('openlp.core.ui.mainwindow.start_zeroconf'), \
+            patch('openlp.core.ui.mainwindow.PluginForm'):
+        return MainWindow()
 
 
 def test_cmd_line_file(main_window):
@@ -341,3 +369,46 @@ def test_change_data_directory(mocked_get_directory, mocked_get_data_path, mocke
     # Clean up
     rmtree(temp_folder)
     rmtree(temp_new_data_folder)
+
+
+def test_restore_current_media_manager_item(main_window_reduced):
+    """
+    Regression test for bug #1152509.
+    """
+    # save current plugin: True; current media plugin: 2
+    main_window_reduced.settings.setValue('advanced/save current plugin', True)
+    main_window_reduced.settings.setValue('advanced/current media plugin', 2)
+
+    # WHEN: Call the restore method.
+    main_window_reduced.restore_current_media_manager_item()
+
+    # THEN: The current widget should have been set.
+    main_window_reduced.media_tool_box.setCurrentIndex.assert_called_with(2)
+
+
+def test_projector_manager_dock_locked(main_window_reduced):
+    """
+    Projector Manager enable UI options -  bug #1390702
+    """
+    # GIVEN: A mocked projector manager dock item:
+    projector_dock = main_window_reduced.projector_manager_dock
+
+    # WHEN: main_window.lock_panel action is triggered
+    main_window_reduced.lock_panel.triggered.emit(True)
+
+    # THEN: Projector manager dock should have been called with disable UI features
+    projector_dock.setFeatures.assert_called_with(0)
+
+
+def test_projector_manager_dock_unlocked(main_window_reduced):
+    """
+    Projector Manager disable UI options -  bug #1390702
+    """
+    # GIVEN: A mocked projector manager dock item:
+    projector_dock = main_window_reduced.projector_manager_dock
+
+    # WHEN: main_window.lock_panel action is triggered
+    main_window_reduced.lock_panel.triggered.emit(False)
+
+    # THEN: Projector manager dock should have been called with enable UI features
+    projector_dock.setFeatures.assert_called_with(7)
