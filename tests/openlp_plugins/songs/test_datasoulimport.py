ac7ba118f92ff76b5d8a34ab7b56af4b35adddb5
Bastian Germann <bastiangermann@fishpost.de>
2023-04-18 05:05:34 +0000

    Add Datasoul song importer

---
+++ tests/openlp_plugins/songs/test_datasoulimport.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+##########################################################################
+# OpenLP - Open Source Lyrics Projection                                 #
+# ---------------------------------------------------------------------- #
+# Copyright (c) 2008-2023 OpenLP Developers                              #
+# ---------------------------------------------------------------------- #
+# This program is free software: you can redistribute it and/or modify   #
+# it under the terms of the GNU General Public License as published by   #
+# the Free Software Foundation, either version 3 of the License, or      #
+# (at your option) any later version.                                    #
+#                                                                        #
+# This program is distributed in the hope that it will be useful,        #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of         #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          #
+# GNU General Public License for more details.                           #
+#                                                                        #
+# You should have received a copy of the GNU General Public License      #
+# along with this program.  If not, see <https://www.gnu.org/licenses/>. #
+##########################################################################
+"""
+This module contains tests for the Datasoul song importer.
+"""
+from tests.helpers.songfileimport import SongImportTestHelper
+from tests.utils.constants import RESOURCE_PATH
+
+
+TEST_PATH = RESOURCE_PATH / 'songs' / 'datasoul'
+
+
+def test_datasoul(mock_settings):
+
+    test_file_import = SongImportTestHelper('DatasoulImport', 'datasoul')
+    test_file_import.setUp()
+    expected = test_file_import.load_external_result_data(TEST_PATH / 'Amazing Grace.json')
+    test_file_import.file_import([TEST_PATH / 'Amazing Grace.song'], expected)
+    test_file_import.tearDown()
