ae0b7760129707f267d35283274da6e4c5b612ad
Raoul Snyman <raoul@snyman.info>
2024-11-01 23:03:51 -0500

    Update the generate resources script to use rcc from Qt6

--- scripts/generate_resources.sh
+++ scripts/generate_resources.sh
@@ -33,6 +33,37 @@
 # working state you may optionally remove openlp/core/resources.py.old
 #
 ###############################################################################
+
+# Dependencies
+#
+# Depending on your platform, you will need to install the following for the "rcc" command:
+#
+# Debian: qt6-base-dev-tools
+# Fedora: pyside6-tools
+
+# Find the "rcc" binary
+COMMANDS="pyside6-rcc rcc /usr/lib/qt6/libexec/rcc /usr/lib/qt6/rcc"
+RCC_CMD=""
+for RCC_CMD in $COMMANDS; do
+    echo $RCC_CMD
+    if command -v $RCC_CMD 2>&1 >/dev/null; then
+        if [ "$RCC_CMD" = "rcc" ]; then
+            RCC_VERSION=`rcc -v | cut -d' ' -f2 | cut -d. -f1`
+            if [ "$RCC_VERSION" = "6" ]; then
+                break
+            fi
+        else
+            break
+        fi
+    fi
+    RCC_CMD=""
+done
+
+if [ "$RCC_CMD" = "" ]; then
+    echo "ERROR: rcc binary not found"
+    exit 1
+fi
+
 # Backup the existing resources
 if [ -f "openlp/core/resources.py" ]; then
     echo "Backup old resources file"
@@ -41,16 +72,16 @@ fi
 
 # Create the new data from the updated qrc
 echo "Generate new resources file"
-pyrcc5 -o openlp/core/resources.py.new resources/images/openlp-2.qrc
+$RCC_CMD -g python -o openlp/core/resources.py.new resources/images/openlp-2.qrc
 
 # Remove patch breaking lines
-echo "Remove 'Created by' line"
-cat openlp/core/resources.py.new | sed '/# Created by: /d' > openlp/core/resources.py
+echo "Remove comments at top of file"
+cat openlp/core/resources.py.new | sed '1,5d' > openlp/core/resources.py
 
 # Patch resources.py to OpenLP coding style
 ARCH=`uname -m`
 echo "Architecture is: $ARCH"
-if [ "$ARCH" == "arm64" ]; then
+if [ "$ARCH" = "arm64" ]; then
     echo "Running ARM64 patch"
     patch --posix -s openlp/core/resources.py scripts/resources.arm64.patch
 else
