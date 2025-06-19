a3552a9ab419f6f4465c7a99c6dd18f709b54ca5
Raoul Snyman <raoul@snyman.info>
2025-06-18 23:57:12 -0500

    Fix #2060 by auto-detecting the attribute

--- tests/openlp_core/api/http_server/test_http.py
+++ tests/openlp_core/api/http_server/test_http.py
@@ -22,23 +22,25 @@
 Functional tests to test the Http Server Class.
 """
 from pathlib import Path
-from unittest.mock import patch
+from unittest.mock import MagicMock, patch
 
-from openlp.core.api.http.server import HttpServer
+from openlp.core.api.http.server import HttpServer, HttpWorker
 from openlp.core.common.registry import Registry
+from openlp.core.common.settings import Settings
 
 
 @patch('openlp.core.api.http.server.HttpWorker')
 @patch('openlp.core.api.http.server.run_thread')
 @patch('openlp.core.api.deploy.AppLocation.get_section_data_path')
-def test_server_start(mocked_get_section_data_path, mocked_run_thread, MockHttpWorker, registry):
+def test_server_start(mocked_get_section_data_path: MagicMock, mocked_run_thread: MagicMock,
+                      MockHttpWorker: MagicMock, registry: Registry):
     """
     Test the starting of the Waitress Server with the disable flag set off
     """
     # GIVEN: A new httpserver and mocked get_section_data_path
     mocked_get_section_data_path.return_value = Path('.')
     # WHEN: I start the server
-    Registry().set_flag('no_web_server', False)
+    registry.set_flag('no_web_server', False)
     server = HttpServer()
     server.bootstrap_post_set_up()
 
@@ -50,7 +52,8 @@ def test_server_start(mocked_get_section_data_path, mocked_run_thread, MockHttpW
 @patch('openlp.core.api.http.server.HttpWorker')
 @patch('openlp.core.api.http.server.run_thread')
 @patch('openlp.core.api.deploy.AppLocation.get_section_data_path')
-def test_server_start_not_required(mocked_get_section_data_path, mocked_run_thread, MockHttpWorker, registry):
+def test_server_start_not_required(mocked_get_section_data_path: MagicMock, mocked_run_thread: MagicMock,
+                                   MockHttpWorker: MagicMock, registry: Registry):
     """
     Test the starting of the Waitress Server with the disable flag set off
     """
@@ -58,10 +61,74 @@ def test_server_start_not_required(mocked_get_section_data_path, mocked_run_thre
     mocked_get_section_data_path.return_value = Path('.')
 
     # WHEN: I start the server
-    Registry().set_flag('no_web_server', True)
+    registry.set_flag('no_web_server', True)
     server = HttpServer()
     server.bootstrap_post_set_up()
 
     # THEN: the api environment should have been created
     assert mocked_run_thread.call_count == 0, 'The qthread should not have have been called'
     assert MockHttpWorker.call_count == 0, 'The http thread should not have been called'
+
+
+@patch('openlp.core.api.http.server.application')
+@patch('openlp.core.api.http.server.create_server')
+@patch('openlp.core.api.http.server.AppLocation.get_section_data_path')
+def test_worker_start(mocked_get_path: MagicMock, mocked_create_server: MagicMock,
+                      mocked_application: MagicMock, registry: Registry, settings: Settings):
+    """Test starting the HTTP worker"""
+    # GIVEN: An HttpWorker object and some mocks
+    settings.setValue('api/ip address', '127.0.0.1')
+    settings.setValue('api/port', '32565')
+    mocked_get_path.return_value = Path('remotes')
+    mocked_server = MagicMock()
+    mocked_create_server.return_value = mocked_server
+
+    # WHEN: The start() method is called
+    worker = HttpWorker(None)
+    with patch.object(worker, 'quit') as mocked_quit:
+        worker.start()
+
+    # THEN: The right calls should have been made
+    assert mocked_application.static_folder == str(Path('remotes') / 'static')
+    mocked_create_server.assert_called_once_with(mocked_application, host='127.0.0.1', port=32565)
+    assert worker.server is mocked_server
+    mocked_server.run.assert_called_once_with()
+    mocked_quit.emit.assert_called_once_with()
+
+
+def test_worker_stop_with_map():
+    """Test that calling stop() on the worker does the right things (with .map)"""
+    # GIVEN: A worker with some mocks
+    worker = HttpWorker(None)
+    worker.server = MagicMock()
+    mock_channel_1 = MagicMock()
+    del mock_channel_1.close
+    mock_channel_2 = MagicMock()
+    worker.server.map.values.return_value = [mock_channel_1, mock_channel_2]
+    del worker.server._map
+
+    # WHEN: stop() is called
+    worker.stop()
+
+    # THEN: The channels should have been closed
+    worker.server.map.values.assert_called_once_with()
+    mock_channel_2.close.assert_called_once_with()
+
+
+def test_worker_stop_with__map():
+    """Test that calling stop() on the worker does the right things (with ._map)"""
+    # GIVEN: A worker with some mocks
+    worker = HttpWorker(None)
+    worker.server = MagicMock()
+    mock_channel_1 = MagicMock()
+    del mock_channel_1.close
+    mock_channel_2 = MagicMock()
+    worker.server._map.values.return_value = [mock_channel_1, mock_channel_2]
+    del worker.server.map
+
+    # WHEN: stop() is called
+    worker.stop()
+
+    # THEN: The channels should have been closed
+    worker.server._map.values.assert_called_once_with()
+    mock_channel_2.close.assert_called_once_with()
